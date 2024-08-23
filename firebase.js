012596f43c75b4e331b4314cab12335494418472
Aashish Vivekanand <aashishvanand@gmail.com>
2024-08-23 10:16:26 +0800

    Removed Secrets from firebase

--- firebase.js
+++ firebase.js
@@ -3,14 +3,14 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBwArCVwu3sDvudkKO5wECRLhoAYmjvNh4",
-    authDomain: "ccreward-7cf93.firebaseapp.com",
-    projectId: "ccreward-7cf93",
-    storageBucket: "ccreward-7cf93.appspot.com",
-    messagingSenderId: "942389628088",
-    appId: "1:942389628088:web:b409508a50487ddea2384d",
-    measurementId: "G-3FTX7LCGBC"
-  };
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
