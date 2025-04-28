01916d1ca2b279a3871d0b102481926a90bc9617
Aashish Vivekanand <aashishvanand@users.noreply.github.com>
2025-04-29 00:58:48 +0800

    Fixed Build issues

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,6 @@ import Script from "next/script";
 import Providers from './providers'
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { baseJsonLd } from '../shared/constants/jsonLd';
-import { motion } from 'framer-motion';
 
 const inter = Inter({ subsets: ['latin'] })
 
