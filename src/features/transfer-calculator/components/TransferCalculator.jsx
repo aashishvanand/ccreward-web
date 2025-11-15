83da1afd6cf9ae095f2786a6ca552c597cc68adc
Aashish Vivekanand <aashishvanand@gmail.com>
2025-11-15 23:56:55 +0800

    fix 🐛: Corrected image URL format to fix broken image display

--- src/features/transfer-calculator/components/TransferCalculator.jsx
+++ src/features/transfer-calculator/components/TransferCalculator.jsx
@@ -273,7 +273,7 @@ const TransferCalculator = () => {
                     <CircularProgress size={24} />
                   ) : logoId ? (
                     <Image
-                      src={`https://imagedelivery.net/o7c7-WjKE1zaslpSuiAT5w/${logoId}/public`}
+                      src={`${logoId}/public`}
                       alt={`${partner.brand_name} logo`}
                       width={40}
                       height={40}
