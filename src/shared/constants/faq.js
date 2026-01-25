2723fc167e0e303ab9e7484811ae5d12a60a2b41
Aashish Vivekanand <aashishvanand@gmail.com>
2026-01-25 11:06:55 +0800

    feat: Implement region-specific routing for top cards and update the supported banks list in the FAQ.

--- src/shared/constants/faq.js
+++ src/shared/constants/faq.js
@@ -1,84 +1,84 @@
 const faqs = [
-    {
-      question: "What is ccreward?",
-      answer:
-        "ccreward is a comprehensive tool designed to help users optimize their credit card rewards across multiple banks and credit cards in India. It helps calculate rewards based on specific Merchant Category Codes (MCCs) and finds the best card for various spending patterns.",
-    },
-    {
-      question: "Which banks are supported?",
-      answer:
-        "ccreward supports major banks including AMEX, AU, Axis, BOB, Federal, Canara, DBS, HDFC, HSBC, ICICI, IDBI, IDFCFirst, IndusInd, Kotak, OneCard, SBI, SC, RBL, YesBank",
-    },
-    {
-      question: "Is ccreward free to use?",
-      answer:
-        "Yes, ccreward is free for personal use. We offer both a web application and an iOS app that you can use to calculate and maximize your credit card rewards.",
-    },
-    {
-      question: "Does ccreward support Android?",
-      answer:
-        "Yes, ccreward is available on both Android and iOS platforms, as well as a web application.",
-    },
-    {
-      question: "What happens when I sign up using an anonymous account?",
-      answer:
-        "If you sign up anonymously, your account is automatically deleted after 30 days, irrespective of whether you’ve added cards or not. This ensures your privacy.",
-    },
-    {
-      question: "Can ccreward track my reward points?",
-      answer:
-        "Not yet. At the moment, ccreward focuses on helping users find the best card for transactions and calculate rewards. Tracking accumulated reward points is a feature we may add in the future.",
-    },
-    {
-      question: "How does ccreward handle my data?",
-      answer:
-        "ccreward values user privacy. If you use Google or Apple Sign-In, we collect only basic info like email.",
-    },
-    {
-      question: "How often is the data on rewards updated?",
-      answer:
-        "We strive to keep reward data as accurate as possible and make regular updates to reflect changes in bank offers and terms.",
-    },
-    {
-      question: "Can ccreward suggest the best credit card to apply for?",
-      answer:
-        "Yes! The 'Suggest Me a Card' feature is in development and will help users identify the top cards based on their spending habits and preferred rewards.",
-    },
-    {
-      question: "Is there an option to share my card portfolio?",
-      answer:
-        "Currently, there is no option to share your portfolio, but users can save and manage all their cards within the app. If there is demand, we may consider adding a secure sharing feature in the future.",
-    },
-    {
-      question: "Can I give feedback on the app?",
-      answer:
-        "Yes, feedback is welcome! You can reach out via the web app or through social media channels to share your experience or suggestions.",
-    },
-    {
-      question: "Is there a limit to how many cards I can add?",
-      answer:
-        "No, you can add as many cards as you like in the app, allowing you to manage and track rewards for multiple credit cards in one place.",
-    },
-    {
-      question: "Does ccreward support international cards?",
-      answer:
-        "Currently, ccreward focuses on Indian credit cards. Support for international cards may be added in future updates.",
-    },
-    {
-      question: "Can ccreward tell me the best card for each transaction?",
-      answer:
-        "Yes, the 'Find Best Card' feature helps you choose the ideal card based on transaction type, category, and amount to maximize your rewards.",
-    },
-    {
-      question: "Does ccreward offer reminders for payment due dates?",
-      answer:
-        "Not currently. ccreward is focused on rewards optimization and does not include payment reminders. This feature may be considered in the future.",
-    },
-    {
-      question: "Can I manually add a card if it is not listed?",
-      answer:
-        "No, currently you cannot manually add cards. However, you can report missing cards, and we’ll prioritize adding them to the app.",
-    },
-  ];
+  {
+    question: "What is ccreward?",
+    answer:
+      "ccreward is a comprehensive tool designed to help users optimize their credit card rewards across multiple banks and credit cards in India. It helps calculate rewards based on specific Merchant Category Codes (MCCs) and finds the best card for various spending patterns.",
+  },
+  {
+    question: "Which banks are supported?",
+    answer:
+      "ccreward supports major banks including AMEX, AU, Axis, BOB, Canara, DBS, Equitas, Federal, HDFC, HSBC, ICICI, IDBI, IDFCFirst, IndusInd, Kotak, OneCard, PNB, RBL, SBI, SBM, SC, Union, YesBank in India and AMEX, BOC, CIMB, Citi, DBS, DCS, GXS, HSBC, Maybank, Maribank, OCBC, POSB, SC, TrustBank, UOB in Singapore",
+  },
+  {
+    question: "Is ccreward free to use?",
+    answer:
+      "Yes, ccreward is free for personal use. We offer both a web application and an iOS app that you can use to calculate and maximize your credit card rewards.",
+  },
+  {
+    question: "Does ccreward support Android?",
+    answer:
+      "Yes, ccreward is available on both Android and iOS platforms, as well as a web application.",
+  },
+  {
+    question: "What happens when I sign up using an anonymous account?",
+    answer:
+      "If you sign up anonymously, your account is automatically deleted after 30 days, irrespective of whether you’ve added cards or not. This ensures your privacy.",
+  },
+  {
+    question: "Can ccreward track my reward points?",
+    answer:
+      "Not yet. At the moment, ccreward focuses on helping users find the best card for transactions and calculate rewards. Tracking accumulated reward points is a feature we may add in the future.",
+  },
+  {
+    question: "How does ccreward handle my data?",
+    answer:
+      "ccreward values user privacy. If you use Google or Apple Sign-In, we collect only basic info like email.",
+  },
+  {
+    question: "How often is the data on rewards updated?",
+    answer:
+      "We strive to keep reward data as accurate as possible and make regular updates to reflect changes in bank offers and terms.",
+  },
+  {
+    question: "Can ccreward suggest the best credit card to apply for?",
+    answer:
+      "Yes! The 'Suggest Me a Card' feature is in development and will help users identify the top cards based on their spending habits and preferred rewards.",
+  },
+  {
+    question: "Is there an option to share my card portfolio?",
+    answer:
+      "Currently, there is no option to share your portfolio, but users can save and manage all their cards within the app. If there is demand, we may consider adding a secure sharing feature in the future.",
+  },
+  {
+    question: "Can I give feedback on the app?",
+    answer:
+      "Yes, feedback is welcome! You can reach out via the web app or through social media channels to share your experience or suggestions.",
+  },
+  {
+    question: "Is there a limit to how many cards I can add?",
+    answer:
+      "No, you can add as many cards as you like in the app, allowing you to manage and track rewards for multiple credit cards in one place.",
+  },
+  {
+    question: "Does ccreward support international cards?",
+    answer:
+      "Currently, ccreward focuses on Indian credit cards. Support for international cards may be added in future updates.",
+  },
+  {
+    question: "Can ccreward tell me the best card for each transaction?",
+    answer:
+      "Yes, the 'Find Best Card' feature helps you choose the ideal card based on transaction type, category, and amount to maximize your rewards.",
+  },
+  {
+    question: "Does ccreward offer reminders for payment due dates?",
+    answer:
+      "Not currently. ccreward is focused on rewards optimization and does not include payment reminders. This feature may be considered in the future.",
+  },
+  {
+    question: "Can I manually add a card if it is not listed?",
+    answer:
+      "No, currently you cannot manually add cards. However, you can report missing cards, and we’ll prioritize adding them to the app.",
+  },
+];
 
-  export default faqs;
+export default faqs;
